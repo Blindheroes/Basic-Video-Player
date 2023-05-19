392e3a16d45386d68321d9cd3dbbbb020a1d2393
Blindheroes <95034324+Blindheroes@users.noreply.github.com>
2023-05-19 10:49:49 +0700

    vanilla

---
+++ controls.js
@@ -0,0 +1,16 @@
+const video = document.querySelector("video")
+
+
+
+function play() {
+    console.log("play")
+    video.play()
+}
+function pause() {
+    console.log("play")
+    video.pause()
+}
+function minimizi() {
+    console.log("play")
+    video.requestPictureInPicture()
+}
